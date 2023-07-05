25a2129eb0dd6ac0135e28077b3723e7f7737385
babub22 <vladlesovoy2@gmail.com>
2023-07-05 16:42:13 +0200

    middleware: Add new `validateRequestBody` MW

---
+++ middleware/validateRequestBody.js
@@ -0,0 +1,13 @@
+function validateRequestBody(validator) {
+  return (req, res, next) => {
+    const { error } = validator(req.body);
+
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+
+    next();
+  };
+}
+
+module.exports = validateRequestBody;
