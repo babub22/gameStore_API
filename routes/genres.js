a61ed86cca24ac03a65d4bc1114b7cb05296bdfe
babub22 <vladlesovoy2@gmail.com>
2023-07-05 16:42:59 +0200

    genres: Add new routing part for `GET /:id`

--- routes/genres.js
+++ routes/genres.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
 const { Genre } = require("../models/genre");
+const objectIdValidator = require("../utils/validators/objectIdValidator");
+const validateRequestBody = require("../middleware/validateRequestBody");
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find();
+  res.send(genres);
+});
+
+router.get("/:id", validateRequestBody(objectIdValidator), async (req, res) => {
+  const { objectId } = req.body;
+
+  const genres = await Genre.findById(objectId);
+
+  if (!genres) {
+    return res.status(404).send("Genre with the specified ID was not found!");
+  }
+
   res.send(genres);
 });
 
