2009471826da40329b989a97b764174e5b0f2d7e
babub22 <vladlesovoy2@gmail.com>
2023-07-06 16:18:50 +0200

    users: Add new `getAdminToken` function

---
+++ tests/utils/getAdminToken.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { User } = require("../../models/user");
+
+function getAdminToken() {
+  const objectId = new mongoose.Types.ObjectId();
+
+  const token = new User({
+    _id: objectId,
+    role: "Admin",
+  }).generateAuthToken();
+
+  return { token, objectId: objectId.toHexString() };
+}
+
+module.exports = getAdminToken;
