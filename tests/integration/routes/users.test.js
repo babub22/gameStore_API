c725dd4caa8df6eedc706611216d601f79cd59ea
babub22 <vladlesovoy2@gmail.com>
2023-07-07 10:29:50 +0200

    users: Write tests for `/singin` routing

--- tests/integration/routes/users.test.js
+++ tests/integration/routes/users.test.js
@@ -4,6 +4,7 @@ const config = require("config");
 const { User } = require("../../../models/user");
 const app = require("../../../index");
 const { pick } = require("lodash");
+const getHashedString = require("../../../utils/bcrypt/getHashedString");
 const request = require("supertest")(app);
 
 const route = "/api/users/";
@@ -15,7 +16,8 @@ const validUserData = {
 };
 
 async function createNewUser() {
-  const newUser = new User(validUserData);
+  const hashedPassword = await getHashedString(validUserData.password);
+  const newUser = new User({ ...validUserData, password: hashedPassword });
 
   await newUser.save();
 
@@ -31,10 +33,9 @@ describe(route, () => {
 
   describe("singup/", () => {
     const singUpRoute = route + "singup";
+    const exec = (userData) => request.post(singUpRoute).send(userData);
 
     describe("POST /", () => {
-      const exec = (userData) => request.post(singUpRoute).send(userData);
-
       test("if invalid data in request, it will return 400", async () => {
         const res = await exec({});
 
@@ -69,6 +70,49 @@ describe(route, () => {
 
         expect(decodedJWT._id).toBe(userInDb._id.toHexString());
       });
+    });
+  });
+
+  describe("singin/", () => {
+    const validLoginData = { email: "example@gmail.com", password: "1234" };
+
+    const singInRoute = route + "singin";
+    const exec = (userData) => request.post(singInRoute).send(userData);
+
+    test("if user with entered email does not exist, it will return 404", async () => {
+      const res = await exec({
+        ...validLoginData,
+        email: "wrongMail@gmail.com",
+      });
+
+      expect(res.status).toBe(404);
+    });
+    test("if user entered an incorrect password, it will return 404", async () => {
+      await createNewUser();
+
+      const res = await exec({ ...validLoginData, password: "54321" });
+
+      expect(res.status).toBe(404);
+    });
+
+    test("if email and password valid, it will return 200", async () => {
+      await createNewUser();
+
+      const res = await exec(validLoginData);
+
+      expect(res.status).toBe(200);
+    });
+    test("if returnrned jwt is valid", async () => {
+      const { userId } = await createNewUser();
+
+      const res = await exec(validLoginData);
+
+      const decodedJWT = jwt.verify(
+        res.header["x-auth-token"],
+        config.get("jwtPrivateKey")
+      );
+
+      expect(decodedJWT._id).toBe(userId);
     });
   });
 });
