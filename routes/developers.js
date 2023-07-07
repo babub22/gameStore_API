ccb07d0aa22b338a04642167cc4791af20bd09c3
babub22 <vladlesovoy2@gmail.com>
2023-07-07 15:56:45 +0200

    developers: Add route for `POST /`

--- routes/developers.js
+++ routes/developers.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
+const admin = require("../middleware/admin");
+const auth = require("../middleware/auth");
+const validateRequestBody = require("../middleware/validateRequestBody");
 const { Developer } = require("../models/developer");
+const developerValidator = require("../utils/validators/developer/developerValidator");
 
 router.get("/", async (req, res) => {
   const developers = await Developer.find();
 
   res.send(developers);
 });
+
+router.post(
+  "/",
+  [auth, admin, validateRequestBody(developerValidator)],
+  async (req, res) => {
+    const newDeveloper = new Developer(req.body);
+
+    await newDeveloper.save();
+
+    res.send(newDeveloper);
+  }
+);
 
 module.exports = router;
