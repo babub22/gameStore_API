9e0d2e99beeca1524688b953e5653779a3ddedee
babub22 <vladlesovoy2@gmail.com>
2023-07-08 16:18:08 +0200

    games: Write tests for `POST /` route

--- tests/integration/routes/games.test.js
+++ tests/integration/routes/games.test.js
@@ -5,9 +5,27 @@ const createNewGenre = require("./utils/createNewGenre");
 const createNewDevelover = require("./utils/createNewDevelover");
 const { Genre } = require("../../../models/genre");
 const { Developer } = require("../../../models/developer");
-const stringToDate = require("../../../utils/stringToDate");
+const { stringToDate } = require("../../../utils/stringToDate");
+const getAdminToken = require("../../utils/getAdminToken");
+const mongoose = require("mongoose");
 
 const route = "/api/games";
+
+async function getValidNewGameParams() {
+  const { developerId } = await createNewDevelover();
+  const { genreId } = await createNewGenre();
+
+  const validNewGameParams = {
+    title: "Game title",
+    price: 20,
+    gameReleaseDate: "30/05/1995",
+    description: new Array(26).join("a"),
+    developerId,
+    genreId,
+  };
+
+  return validNewGameParams;
+}
 
 async function createNewGame() {
   const { newDeveloper } = await createNewDevelover();
@@ -19,7 +37,7 @@ async function createNewGame() {
     title: "Game title",
     price: 20,
     gameReleaseDate,
-    decription: new Array(26).join("a"),
+    description: new Array(26).join("a"),
     genre: newGenre,
     developer: newDeveloper,
   });
@@ -54,6 +72,52 @@ describe(route, () => {
         const res = await exec();
 
         expect(res.body.some((game) => game._id === gameId)).toBe(true);
+      });
+    });
+  });
+
+  describe("POST", () => {
+    const { token } = getAdminToken();
+
+    describe("/", () => {
+      const exec = (validGame) =>
+        request.post(route).set("x-auth-token", token).send(validGame);
+
+      test("if developer for provided developerId does not exist, it will return 404", async () => {
+        const validNewGameParams = await getValidNewGameParams();
+        const res = await exec({
+          ...validNewGameParams,
+          developerId: new mongoose.Types.ObjectId(),
+        });
+
+        expect(res.status).toBe(404);
+      });
+
+      test("if genre for provided genreId does not exist, it will return 404", async () => {
+        const validNewGameParams = await getValidNewGameParams();
+        const res = await exec({
+          ...validNewGameParams,
+          genreId: new mongoose.Types.ObjectId(),
+        });
+
+        expect(res.status).toBe(404);
+      });
+
+      test("if wrong data format, it will return 400", async () => {
+        const validNewGameParams = await getValidNewGameParams();
+        const res = await exec({
+          ...validNewGameParams,
+          gameReleaseDate: "29 05 1995",
+        });
+
+        expect(res.status).toBe(400);
+      });
+
+      test("if valid request, it will return 200", async () => {
+        const validNewGameParams = await getValidNewGameParams();
+        const res = await exec(validNewGameParams);
+
+        expect(res.status).toBe(200);
       });
     });
   });
