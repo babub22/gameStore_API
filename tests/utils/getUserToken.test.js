d8c8e61422e2f7bbfb49632541c6dc3bb754595c
babub22 <vladlesovoy2@gmail.com>
2023-07-08 16:47:14 +0200

    utils: Add new test case in `getUserToken` test for username

--- tests/utils/getUserToken.test.js
+++ tests/utils/getUserToken.test.js
@@ -9,11 +9,15 @@ describe("getUserToken", () => {
 
   test("if token payload contains the correct data", () => {
     expect(Object.keys(decoded)).toEqual(
-      expect.arrayContaining(["_id", "role"])
+      expect.arrayContaining(["_id", "role", "username"])
     );
   });
 
   test("if token payload contains user role", () => {
     expect(decoded.role).toBe("User");
+  });
+
+  test("if token payload contains username Vlad", () => {
+    expect(decoded.username).toBe("Vlad");
   });
 });
