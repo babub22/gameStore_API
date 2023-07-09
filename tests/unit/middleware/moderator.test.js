98b760d86058a7c1952cee0188b8e45499cd4251
babub22 <vladlesovoy2@gmail.com>
2023-07-09 17:34:09 +0200

    middleware: Write tests for `moderator`

---
+++ tests/unit/middleware/moderator.test.js
@@ -0,0 +1,30 @@
+const getModeratorToken = require("../../utils/getModeratorToken");
+const moderator = require("../../../middleware/moderator");
+const callMockedMiddleware = require("./utils/callMockedMiddleware");
+
+describe("moderator", () => {
+  test("if provided user is not admin, it will return 403", () => {
+    const { token } = getModeratorToken();
+    const { responseStatus } = callMockedMiddleware(token, moderator);
+
+    expect(responseStatus).toEqual(403);
+  });
+  test("if provided user is not moderator, it will return 403", () => {
+    const { token } = getModeratorToken();
+    const { responseStatus } = callMockedMiddleware(token, moderator);
+
+    expect(responseStatus).toEqual(403);
+  });
+  test("if provided user is moderator, next() will be run", () => {
+    const { token } = getModeratorToken();
+    const { next } = callMockedMiddleware(token, moderator);
+
+    expect(next).toHaveBeenCalled();
+  });
+  test("if provided user is admin, next() will be run", () => {
+    const { token } = getModeratorToken();
+    const { next } = callMockedMiddleware(token, moderator);
+
+    expect(next).toHaveBeenCalled();
+  });
+});
