15fb8ca92bfb2e179659e8784d60c5ad88c9b600
babub22 <vladlesovoy2@gmail.com>
2023-07-09 17:32:28 +0200

    utils: Add new `getModeratorToken` function

---
+++ tests/utils/getModeratorToken.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { User } = require("../../models/user");
+
+function getModeratorToken() {
+  const objectId = new mongoose.Types.ObjectId();
+
+  const token = new User({
+    _id: objectId,
+    name: "Vlad",
+    role: "Moderator",
+    isReviewer: false,
+  }).generateAuthToken();
+
+  return { token, objectId: objectId.toHexString() };
+}
+
+module.exports = getModeratorToken;
