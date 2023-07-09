7fd2791a14db8c084d774b0cb8a664a677f5325a
babub22 <vladlesovoy2@gmail.com>
2023-07-09 17:33:56 +0200

    middleware: Add new `moderator`

---
+++ middleware/moderator.js
@@ -0,0 +1,9 @@
+function moderator(req, res, next) {
+  if (["Admin", "Moderator"].includes(req.user.role)) {
+    return res.status(403).send("Access denied");
+  }
+
+  next();
+}
+
+module.exports = moderator;
