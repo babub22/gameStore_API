518a00d9ad8e1eb16c4c185b6e1881aa7846a592
babub22 <vladlesovoy2@gmail.com>
2023-07-09 17:29:58 +0200

    middleware: Refactor test for `admin`

--- tests/unit/middleware/admin.test.js
+++ tests/unit/middleware/admin.test.js
@@ -1,26 +1,18 @@
 const admin = require("../../../middleware/admin");
 const getAdminToken = require("../../utils/getAdminToken");
 const getUserToken = require("../../utils/getUserToken");
-const auth = require("../../../middleware/auth");
-const getMiddlewareParams = require("./utils/getMiddlewareParams");
+const callMockedMiddleware = require("./utils/callMockedMiddleware");
 
 describe("admin", () => {
   test("if provided user is not admin, it will return 403", () => {
     const { token } = getUserToken();
-    const { req, res, next } = getMiddlewareParams(token);
+    const { responseStatus } = callMockedMiddleware(token, admin);
 
-    auth(req, res, next);
-    admin(req, res, next);
-
-    const responseStatus = res.status.mock.calls[0][0];
     expect(responseStatus).toEqual(403);
   });
   test("if provided user is admin, next() will be run", () => {
     const { token } = getAdminToken();
-    const { req, res, next } = getMiddlewareParams(token);
-
-    auth(req, res, next);
-    admin(req, res, next);
+    const { next } = callMockedMiddleware(token, admin);
 
     expect(next).toHaveBeenCalled();
   });
