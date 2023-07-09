456195759f0d12794aae4caf9e1ff49f34f4f1be
babub22 <vladlesovoy2@gmail.com>
2023-07-09 16:39:07 +0200

    reviews: Add new route for `POST/`

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,10 @@ const { Review } = require("../models/review");
 const objectIdValidator = require("../utils/validators/objectIdValidator");
 const router = require("express").Router();
 const validateRequestParams = require("../middleware/validateRequestParams");
+const auth = require("../middleware/auth");
+const { Game } = require("../models/game");
+const reviewValidator = require("../utils/validators/review/reviewValidator");
+const validateRequestBody = require("../middleware/validateRequestBody");
 
 router.get(
   "/game/:objectId",
@@ -22,6 +26,30 @@ router.get(
 
     const reviews = await Review.getReviewsByAuthorId(authorId);
     res.send(reviews);
+  }
+);
+
+router.post(
+  "/",
+  [auth, validateRequestBody(reviewValidator)],
+  async (req, res) => {
+    const { gameId } = req.body;
+
+    const game = await Game.findById(gameId);
+
+    if (!game) {
+      return res.status(404).send("This game does not exist in the database!");
+    }
+
+    const newReview = new Review({
+      ...req.body,
+      author: { ...req.user },
+      game,
+    });
+
+    await newReview.save();
+
+    res.send(newReview);
   }
 );
 
