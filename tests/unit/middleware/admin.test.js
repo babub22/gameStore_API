63f8801d7afc48c8e38308d5db439b69eb993feb
babub22 <vladlesovoy2@gmail.com>
2023-07-13 11:24:06 +0200

    middleware: Restructure test for `admin`

--- tests/unit/middleware/admin.test.js
+++ tests/unit/middleware/admin.test.js
@@ -2,17 +2,20 @@ const admin = require("../../../middleware/admin");
 const getAdminToken = require("../../utils/getAdminToken");
 const getUserToken = require("../../utils/getUserToken");
 const callMockedMiddleware = require("./utils/callMockedMiddleware");
+const getHeaderObject = require("./utils/getHeaderObject");
 
 describe("admin", () => {
   test("if provided user is not admin, it will return 403", () => {
     const { token } = getUserToken();
-    const { responseStatus } = callMockedMiddleware(token, admin);
+    const headerObject = getHeaderObject(token);
+    const { responseStatus } = callMockedMiddleware(headerObject, admin);
 
     expect(responseStatus).toEqual(403);
   });
   test("if provided user is admin, next() will be run", () => {
     const { token } = getAdminToken();
-    const { next } = callMockedMiddleware(token, admin);
+    const headerObject = getHeaderObject(token);
+    const { next } = callMockedMiddleware(headerObject, admin);
 
     expect(next).toHaveBeenCalled();
   });
