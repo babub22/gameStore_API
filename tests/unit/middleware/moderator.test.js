c2c04ef7818fca090534d7dcaafa957eb82c675f
babub22 <vladlesovoy2@gmail.com>
2023-07-13 11:23:48 +0200

    middleware: Restructure test for `moderator`

--- tests/unit/middleware/moderator.test.js
+++ tests/unit/middleware/moderator.test.js
@@ -2,35 +2,46 @@ const getModeratorToken = require("../../utils/getModeratorToken");
 const moderator = require("../../../middleware/moderator");
 const callMockedMiddleware = require("./utils/callMockedMiddleware");
 const getUserToken = require("../../utils/getUserToken");
+const getHeaderObject = require("./utils/getHeaderObject");
 
 describe("moderator", () => {
   test("if provided user is not admin, it will return 403", () => {
     const { token } = getModeratorToken();
-    const { responseStatus } = callMockedMiddleware(token, moderator);
+
+    const headerObject = getHeaderObject(token);
+    const { responseStatus } = callMockedMiddleware(headerObject, moderator);
 
     expect(responseStatus).toEqual(403);
   });
   test("if provided user is not moderator, it will return 403", () => {
     const { token } = getModeratorToken();
-    const { responseStatus } = callMockedMiddleware(token, moderator);
+
+    const headerObject = getHeaderObject(token);
+    const { responseStatus } = callMockedMiddleware(headerObject, moderator);
 
     expect(responseStatus).toEqual(403);
   });
   test("if provided user is moderator, next() will be run", () => {
     const { token } = getModeratorToken();
-    const { next } = callMockedMiddleware(token, moderator);
+
+    const headerObject = getHeaderObject(token);
+    const { next } = callMockedMiddleware(headerObject, moderator);
 
     expect(next).toHaveBeenCalled();
   });
   test("if provided user is admin, next() will be run", () => {
     const { token } = getModeratorToken();
-    const { next } = callMockedMiddleware(token, moderator);
+
+    const headerObject = getHeaderObject(token);
+    const { next } = callMockedMiddleware(headerObject, moderator);
 
     expect(next).toHaveBeenCalled();
   });
   test("if provided user have user role, next() will be run", () => {
     const { token } = getUserToken();
-    const { next } = callMockedMiddleware(token, moderator);
+
+    const headerObject = getHeaderObject(token);
+    const { next } = callMockedMiddleware(headerObject, moderator);
 
     expect(next).toHaveBeenCalled();
   });
