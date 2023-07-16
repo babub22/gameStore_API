23cf608aeb8607c7c1051c272c8d923d502a32b7
babub22 <vladlesovoy2@gmail.com>
2023-07-16 18:40:59 +0200

    models: Restructure `User` model file

--- models/user/user.js
+++ models/user/user.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const compareHashedStrings = require("../../utils/bcrypt/compareHashedStrings");
-const { pick } = require("lodash");
-const getHashedString = require("../../utils/bcrypt/getHashedString");
 const userStatusSchema = require("./userStatusSchema");
+const validateSignin = require("./statics/validateSignin");
+const validateSingup = require("./statics/validateSingup");
+const generateAuthToken = require("./methods/generateAuthToken");
+const blockUserById = require("./statics/blockUserById");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,95 +35,11 @@ const userSchema = new mongoose.Schema({
   userStatus: { type: userStatusSchema, default: { userStatusSchema } },
 });
 
-userSchema.statics.blockUserById = async function ({
-  userId,
-  currentUser,
-  reason,
-}) {
-  const user = await this.findByIdAndUpdate(
-    userId,
-    {
-      userStatus: {
-        status: "Blocked",
-        blockingInfo: { reason, blockedBy: currentUser },
-      },
-    },
-    { new: true }
-  );
+userSchema.methods.generateAuthToken = generateAuthToken;
 
-  if (!user) {
-    return {
-      isValidRequest: false,
-      body: { status: 404, message: "This user doesnt exist!" },
-    };
-  }
-
-  return { isValidRequest: true, body: user };
-};
-
-userSchema.methods.generateAuthToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      name: this.name,
-      role: this.role ?? "User",
-      isReviewer: this.isReviewer,
-    },
-    config.get("jwtPrivateKey")
-  );
-};
-
-userSchema.statics.validateSignin = async function ({ password, email }) {
-  const user = await this.findOne({ email });
-
-  if (!user) {
-    return { status: 404, message: "User with this mail doesnt exist!" };
-  }
-
-  const isBlockedUser = user.userStatus.status === "Blocked";
-
-  if (isBlockedUser) {
-    const { blockedBy } = user.userStatus.blockingInfo;
-
-    return {
-      status: 403,
-      message: `You have been blocked by ${blockedBy.name}, you can no longer sing in to your account!`,
-    };
-  }
-
-  const isValidPassword = await compareHashedStrings(password, user.password);
-
-  if (!isValidPassword) {
-    return { status: 404, message: "Password is wrong!" };
-  }
-
-  const token = await user.generateAuthToken();
-
-  return { token, name: user.name };
-};
-
-userSchema.statics.validateSingup = async function (singupData) {
-  const { email, password } = singupData;
-
-  const user = await this.findOne({ email });
-
-  if (user) {
-    return { status: 400, message: "This user already exist!" };
-  }
-
-  const hashedPassword = await getHashedString(password);
-
-  const newUser = new this({
-    ...singupData,
-    password: hashedPassword,
-  });
-
-  await newUser.save();
-
-  const token = newUser.generateAuthToken();
-
-  return { token, newUser: pick(newUser, ["_id", "name", "email"]) };
-};
+userSchema.statics.blockUserById = blockUserById;
+userSchema.statics.validateSignin = validateSignin;
+userSchema.statics.validateSingup = validateSingup;
 
 const User = mongoose.model("User", userSchema);
 
