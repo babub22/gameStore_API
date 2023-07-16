d72b8819fb13344849d1baae9c3caab8a00484a5
babub22 <vladlesovoy2@gmail.com>
2023-07-16 12:39:44 +0200

    setup: Do when we run tests log only to the console

--- setup/logging.js
+++ setup/logging.js
@@ -1,6 +1,11 @@
 const winston = require("winston");
 
 function logging() {
+  if (process.env.NODE_ENV === "test") {
+    winston.add(new winston.transports.Console())
+    return;
+  }
+
   winston.add(new winston.transports.File({ filename: "logfile.log" }));
 
   winston.exceptions.handle(
