2e78548332430b7069853704afab2f408053fcc7
babub22 <vladlesovoy2@gmail.com>
2023-07-16 18:38:55 +0200

    models: Move `generateAuthToken` to a separate file

---
+++ models/user/methods/generateAuthToken.js
@@ -0,0 +1,14 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+module.exports = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      role: this.role ?? "User",
+      isReviewer: this.isReviewer,
+    },
+    config.get("jwtPrivateKey")
+  );
+};
