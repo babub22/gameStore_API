c052425a7b29018ea4737cc45ff5dbdad2d66cec
babub22 <vladlesovoy2@gmail.com>
2023-07-16 12:35:36 +0200

    utils: Change path to a `User` model in `getAdminToken` function

--- tests/utils/getAdminToken.js
+++ tests/utils/getAdminToken.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { User } = require("../../models/user");
+const { User } = require("../../models/user/user");
 
 function getAdminToken() {
   const objectId = new mongoose.Types.ObjectId();
