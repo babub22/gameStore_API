8e39b32419646f0a7150c08f0f325068ac28c4a1
babub22 <vladlesovoy2@gmail.com>
2023-07-18 10:17:12 +0200

    models: Change setup configuration for `blockUserById` tests

--- tests/integration/models/user/blockUserById.test.js
+++ tests/integration/models/user/blockUserById.test.js
@@ -5,6 +5,7 @@ const logging = require("../../../../setup/logging");
 const getAdminToken = require("../../../utils/getAdminToken");
 const decodeToken = require("../../../../utils/decodeToken");
 const getHexedObjectId = require("../../../../utils/getHexedObjectId");
+const dbDisconnection = require("../../../../setup/dbDisconnection");
 
 describe("User.blockUserById", () => {
   beforeAll(() => {
@@ -12,7 +13,7 @@ describe("User.blockUserById", () => {
     dbConnection();
   });
   afterAll(() => {
-    dbConnection();
+    dbDisconnection();
   });
 
   afterEach(async () => {
@@ -37,25 +38,18 @@ describe("User.blockUserById", () => {
   test("if valid request, it will return 200", async () => {
     const { userId } = await createNewUser();
 
-    const { resultBody } = await await User.blockUserById({
+    const { isValidRequest } = await User.blockUserById({
       userId,
       currentUser: decodedJWT,
       reason: "Test test",
     });
-    const { isValidRequest } = resultBody;
 
     expect(isValidRequest).toBe(true);
   });
   test("if valid request, it will return user document", async () => {
     const { userId } = await createNewUser();
 
-    await User.blockUserById({
-      userId,
-      currentUser: decodedJWT,
-      reason: "Test test",
-    });
-
-    const { resultBody } = await await User.blockUserById({
+    const { resultBody } = await User.blockUserById({
       userId,
       currentUser: decodedJWT,
       reason: "Test test",
