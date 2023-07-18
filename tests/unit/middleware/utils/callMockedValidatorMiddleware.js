325a5ca3e91b8059c05e973828b3e604b558bd16
babub22 <vladlesovoy2@gmail.com>
2023-07-18 10:51:00 +0200

    utils: Change behaviour of `callMockedValidatorMiddleware` function

--- tests/unit/middleware/utils/callMockedValidatorMiddleware.js
+++ tests/unit/middleware/utils/callMockedValidatorMiddleware.js
@@ -1,10 +1,10 @@
 const objectIdValidator = require("../../../../utils/validators/objectIdValidator");
 const getMiddlewareParams = require("./getMiddlewareParams");
 
-function callMockedValidatorMiddleware(requestParams, validatorMiddleware) {
+function callMockedValidatorMiddleware(requestParams, validatorMiddleware, validator=objectIdValidator) {
   const { req, res, next } = getMiddlewareParams(requestParams);
 
-  const middleware = validatorMiddleware(objectIdValidator);
+  const middleware = validatorMiddleware(validator);
   middleware(req, res, next);
 
   return { res, next };
