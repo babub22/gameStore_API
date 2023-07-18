7f1cae82b3fbf387f92d9f41001489a3809d4e2e
babub22 <vladlesovoy2@gmail.com>
2023-07-18 10:30:11 +0200

    middleware: Add new `validateRequestQuery`

---
+++ middleware/validateRequestQuery.js
@@ -0,0 +1,13 @@
+function validateRequestQuery(validator) {
+  return (req, res, next) => {
+    const { error } = validator(req.query);
+
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+
+    next();
+  };
+}
+
+module.exports = validateRequestQuery;
