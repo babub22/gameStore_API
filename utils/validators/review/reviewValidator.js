d90d4af18a458dc6c86261569734efe1f03d0c6e
babub22 <vladlesovoy2@gmail.com>
2023-07-18 10:28:30 +0200

    utils: Change behaviour of `reviewValidator` validator

--- utils/validators/review/reviewValidator.js
+++ utils/validators/review/reviewValidator.js
@@ -3,7 +3,6 @@ Joi.objectId = require("joi-objectid")(Joi);
 
 function reviewValidator(review) {
   const schema = Joi.object({
-    gameId: Joi.objectId().required(),
     text: Joi.string().min(3).required(),
     gameScore: Joi.number().min(0).max(10).required(),
   });
