1ab3545ccfc34097b3b2aa4de1e16c065a5ba2f4
babub22 <vladlesovoy2@gmail.com>
2023-07-19 22:01:27 +0200

    utils: Add new response object `YOU_HAVE_BEEN_BLOCKED_BY`

---
+++ utils/responseObjects/users/YOU_HAVE_BEEN_BLOCKED_BY.js
@@ -0,0 +1,6 @@
+const YOU_HAVE_BEEN_BLOCKED_BY = {
+  status: 403,
+  message: `You have been blocked by ${this.name}, you can no longer sing in to your account!`,
+};
+
+module.exports = YOU_HAVE_BEEN_BLOCKED_BY;
