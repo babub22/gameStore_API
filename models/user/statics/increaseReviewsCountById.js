7f0ae422f226a2a2d6d7032a477b6561a26bced7
babub22 <vladlesovoy2@gmail.com>
2023-07-19 22:12:17 +0200

    models: Restructure `increaseReviewsCountById` method

--- models/user/statics/increaseReviewsCountById.js
+++ models/user/statics/increaseReviewsCountById.js
@@ -1,8 +1,8 @@
-const getResultObject = require("../../../utils/getResultObject");
-const USER_DOES_NOT_EXIST = require("../../../utils/responseMessages/USER_DOES_NOT_EXIST");
+const getResultLikeResponseObject = require("../../../utils/getResultLikeResponseObject");
+const USER_DOES_NOT_EXISTS = require("../../../utils/responseObjects/users/USER_DOES_NOT_EXISTS");
 
 module.exports = async function ({ userId: userId, reviewsNumber = 1 }) {
-  const user = await this.findOneAndUpdate(
+  const userWithIncreasedReviewsCount = await this.findOneAndUpdate(
     { _id: userId },
     {
       $inc: { reviewsCount: reviewsNumber },
@@ -10,20 +10,15 @@ module.exports = async function ({ userId: userId, reviewsNumber = 1 }) {
     { new: true, setDefaultsOnInsert: true }
   );
 
-  let result;
-
-  if (!user) {
-    result = getResultObject(false, {
-      status: 404,
-      message: USER_DOES_NOT_EXIST,
-    });
-
-    return result;
+  async function setIsReviewerAndSaveUser() {
+    userWithIncreasedReviewsCount.isReviewer =
+      userWithIncreasedReviewsCount.reviewsCount > 30;
+    await userWithIncreasedReviewsCount.save();
   }
 
-  user.isReviewer = user.reviewsCount > 30;
-  await user.save();
-
-  result = getResultObject(true, user);
-  return result;
+  return getResultLikeResponseObject({
+    result: userWithIncreasedReviewsCount,
+    errorObject: USER_DOES_NOT_EXISTS,
+    fn: setIsReviewerAndSaveUser,
+  });
 };
