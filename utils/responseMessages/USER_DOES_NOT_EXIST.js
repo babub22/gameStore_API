9812c1cbe5d324fb8e60f1b971bd32aa0f903e94
babub22 <vladlesovoy2@gmail.com>
2023-07-19 21:25:31 +0200

    utils: Move `USER_DOES_NOT_EXISTS` in `responseObject/users` folder

--- utils/responseMessages/USER_DOES_NOT_EXIST.js
+++
@@ -1,3 +0,0 @@
-const USER_DOES_NOT_EXIST = "This user doesnt exist!";
-
-module.exports = USER_DOES_NOT_EXIST;
---
+++ utils/responseObjects/users/USER_DOES_NOT_EXISTS.js
@@ -0,0 +1,6 @@
+const USER_DOES_NOT_EXISTS = {
+  status: 404,
+  message: "This user doesnt exist!",
+};
+
+module.exports = USER_DOES_NOT_EXISTS;
