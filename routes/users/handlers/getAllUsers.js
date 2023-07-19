74897703bdb99a168d71b04db5385330b79b15f8
babub22 <vladlesovoy2@gmail.com>
2023-07-19 22:15:02 +0200

    users: Move handler of `GET/` route to a separate file

---
+++ routes/users/handlers/getAllUsers.js
@@ -0,0 +1,23 @@
+const { User } = require("../../../models/user/user");
+
+async function getAllUsers(req, res) {
+  const { sortBy, limit, role } = req.query;
+
+  let query = {
+    find: { "userStatus.status": "Active" },
+    limit: Number(limit),
+    sortBy: sortBy === "reviewsCount" ? `-${sortBy}` : `${sortBy}`,
+  };
+
+  if (role === "Reviewer") {
+    query.find.isReviewer = true;
+  } else if (role) {
+    query.find.role = role;
+  }
+
+  const sortedUsers = await User.findUsersAndSortByQuery({query});
+
+  res.send(sortedUsers);
+}
+
+module.exports = getAllUsers;
