d9b978b90fb73aa7f0522335d468a65ec51e584e
babub22 <vladlesovoy2@gmail.com>
2023-07-19 21:24:56 +0200

    utils: Move `REVIEW_DOES_NOT_EXISTS` in `responseObject/reviews` folder

--- utils/responseMessages/REVIEW_DOES_NOT_EXIST.js
+++
@@ -1,3 +0,0 @@
-const REVIEW_DOES_NOT_EXIST = "This review does not exist!";
-
-module.exports = REVIEW_DOES_NOT_EXIST;
---
+++ utils/responseObjects/reviews/REVIEW_DOES_NOT_EXISTS.js
@@ -0,0 +1,3 @@
+const REVIEW_DOES_NOT_EXISTS = "This review does not exist!";
+
+module.exports = REVIEW_DOES_NOT_EXISTS;
