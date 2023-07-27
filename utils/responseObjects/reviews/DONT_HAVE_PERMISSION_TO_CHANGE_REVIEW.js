fb6bbeb93b4e04a2c095537ec0e5f8088e79fac7
babub22 <vladlesovoy2@gmail.com>
2023-07-27 22:11:38 +0200

    utils: Create new response object `DONT_HAVE_PERMISSION_TO_CHANGE_REVIEW`

---
+++ utils/responseObjects/reviews/DONT_HAVE_PERMISSION_TO_CHANGE_REVIEW.js
@@ -0,0 +1,6 @@
+const DONT_HAVE_PERMISSION_TO_CHANGE_REVIEW = {
+  status: 403,
+  message: "You dont have permission to change this review!",
+};
+
+module.exports = DONT_HAVE_PERMISSION_TO_CHANGE_REVIEW;
