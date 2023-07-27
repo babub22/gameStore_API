2f0740a36b42cad15a4383fbc9fba7a133516c5f
babub22 <vladlesovoy2@gmail.com>
2023-07-27 22:19:20 +0200

    reviews: Move handler for `POST/:id/like` into `likeReview`

---
+++ routes/reviews/handlers/likeReview.js
@@ -0,0 +1,29 @@
+const { Review } = require("../../../models/review/review");
+const REVIEW_DOES_NOT_EXISTS = require("../../../utils/responseObjects/reviews/REVIEW_DOES_NOT_EXISTS");
+
+async function likeReview(req, res) {
+  const {
+    params: { objectId: reviewId },
+    user,
+  } = req;
+
+  const review = await Review.findById(reviewId);
+
+  if (!review) {
+    const { status, message } = REVIEW_DOES_NOT_EXISTS;
+    return res.status(status).send(message);
+  }
+
+  const isUserAlreadyPutLikeOnThisReview =
+    review.likes.checkIfThisUserAlreadyPutLike(user);
+
+  isUserAlreadyPutLikeOnThisReview
+    ? review.likes.decreaseLikesByOne(user)
+    : review.likes.increaseLikesByOne(user);
+
+  const saved = await review.save();
+
+  res.send(saved);
+}
+
+module.exports = likeReview;
