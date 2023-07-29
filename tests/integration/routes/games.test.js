2cda02f270ec2458da16a9b7e24660a16bf88358
babub22 <vladlesovoy2@gmail.com>
2023-07-29 21:24:58 +0200

    games: Add tests for `DELETE/:gameId`

--- tests/integration/routes/games.test.js
+++ tests/integration/routes/games.test.js
@@ -1,17 +1,17 @@
 const { Game } = require("../../../models/game/game");
 const server = require("../../../index");
 const request = require("supertest")(server);
-const createNewGenre = require("./utils/createNewGenre");
-const createNewDevelover = require("./utils/createNewDevelover");
 const { Genre } = require("../../../models/genre");
 const { Developer } = require("../../../models/developer");
 const getAdminToken = require("../../utils/getAdminToken");
 const mongoose = require("mongoose");
-const createNewGame = require("./utils/createNewGame");
+const { createNewGame } = require("./utils/createNewGame");
 const decodeToken = require("../../../utils/decodeToken");
 const dbDisconnection = require("../../../setup/dbDisconnection");
+const getHexedObjectId = require("../../../utils/getHexedObjectId");
+const getValidNewGameRequestBody = require("./utils/getValidNewGameRequestBody");
 
-const route = "/api/games";
+const route = "/api/games/";
 
 describe(route, () => {
   afterAll(() => {
@@ -36,7 +36,7 @@ describe(route, () => {
       });
 
       test("if returned array of documents contain created game", async () => {
-        const { gameId } = await createNewGame(fakeDate);
+        const { gameId } = await createNewGame();
 
         const res = await exec();
 
@@ -53,7 +53,7 @@ describe(route, () => {
         request.post(route).set("x-auth-token", token).send(validGame);
 
       test("if developer for provided developerId does not exist, it will return 404", async () => {
-        const validNewGameParams = await getValidNewGameParams();
+        const validNewGameParams = await getValidNewGameRequestBody();
         const res = await exec({
           ...validNewGameParams,
           developerId: new mongoose.Types.ObjectId(),
@@ -63,7 +63,7 @@ describe(route, () => {
       });
 
       test("if genre for provided genreId does not exist, it will return 404", async () => {
-        const validNewGameParams = await getValidNewGameParams();
+        const validNewGameParams = await getValidNewGameRequestBody();
         const res = await exec({
           ...validNewGameParams,
           genreId: new mongoose.Types.ObjectId(),
@@ -73,7 +73,7 @@ describe(route, () => {
       });
 
       test("if wrong date, it will return 400", async () => {
-        const validNewGameParams = await getValidNewGameParams();
+        const validNewGameParams = await getValidNewGameRequestBody();
         const res = await exec({
           ...validNewGameParams,
           releaseDate: "29 05 1995",
@@ -83,14 +83,14 @@ describe(route, () => {
       });
 
       test("if valid request, it will return 200", async () => {
-        const validNewGameParams = await getValidNewGameParams();
+        const validNewGameParams = await getValidNewGameRequestBody();
         const res = await exec(validNewGameParams);
 
         expect(res.status).toBe(201);
       });
 
       test("if document contain addedBy property", async () => {
-        const validNewGameParams = await getValidNewGameParams();
+        const validNewGameParams = await getValidNewGameRequestBody();
         const res = await exec(validNewGameParams);
 
         const decodedJWT = decodeToken(token);
@@ -99,22 +99,41 @@ describe(route, () => {
       });
     });
   });
+
+  describe("DELETE", () => {
+    const { token } = getAdminToken();
+    describe("/:gameId", () => {
+      const exec = (gameId) =>
+        request.delete(route + gameId).set("x-auth-token", token);
+
+      test("if valid request, it will send 200", async () => {
+        const { gameId } = await createNewGame();
+
+        const res = await exec(gameId);
+        expect(res.status).toBe(200);
+      });
+      test("if valid request, it will send deleted document", async () => {
+        const { gameId } = await createNewGame();
+
+        const res = await exec(gameId);
+        expect(res.body._id).toEqual(gameId);
+      });
+      test("if document was deleted", async () => {
+        const { gameId } = await createNewGame();
+
+        await exec(gameId);
+
+        const deletedGameInDB = await Game.findById(gameId);
+
+        expect(deletedGameInDB).toBeNull();
+      });
+      test("if game for provided gameId doesnt exists, it will return 404", async () => {
+        const gameId = getHexedObjectId();
+
+        const res = await exec(gameId);
+
+        expect(res.status).toEqual(404);
+      });
+    });
+  });
 });
-
-const fakeDate = Date.parse("1995-05-30");
-
-async function getValidNewGameParams() {
-  const { developerId } = await createNewDevelover();
-  const { genreId } = await createNewGenre();
-
-  const validNewGameParams = {
-    title: "Game title",
-    price: 20,
-    releaseDate: fakeDate,
-    description: new Array(26).join("a"),
-    developerId,
-    genreId,
-  };
-
-  return validNewGameParams;
-}
