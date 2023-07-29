d467b26bdfb3e507cdf87629a9df7c5b8273e4e6
babub22 <vladlesovoy2@gmail.com>
2023-07-29 17:13:01 +0200

    models: Change behaviour of `increaseReviewsCountById` method

--- models/user/statics/increaseReviewsCountById.js
+++ models/user/statics/increaseReviewsCountById.js
@@ -1,11 +1,17 @@
 const getResultLikeResponseObject = require("../../../utils/getResultLikeResponseObject");
 const USER_DOES_NOT_EXISTS = require("../../../utils/responseObjects/users/USER_DOES_NOT_EXISTS");
 
-module.exports = async function ({ userId: userId, reviewsNumber = 1 }) {
+module.exports = async function ({ userId, reviewsNumber = 1 }) {
+  const reviewsCount =
+    (await this.findById(userId).select("reviewsCount -_id"))?.toObject()
+      .reviewsCount || 0;
+
+  const changedReviewsCount = reviewsCount + reviewsNumber;
+
   const userWithIncreasedReviewsCount = await this.findOneAndUpdate(
     { _id: userId },
     {
-      $inc: { reviewsCount: reviewsNumber },
+      reviewsCount: changedReviewsCount > 0 ? changedReviewsCount : 0,
     },
     { new: true, setDefaultsOnInsert: true }
   );
