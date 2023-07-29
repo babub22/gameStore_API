db23a4569d71e05b2353e1ed32b98bd0f3ad44f7
babub22 <vladlesovoy2@gmail.com>
2023-07-29 21:26:01 +0200

    games: Add tests for `PUT/:gameId`

--- tests/integration/routes/games.test.js
+++ tests/integration/routes/games.test.js
@@ -5,7 +5,7 @@ const { Genre } = require("../../../models/genre");
 const { Developer } = require("../../../models/developer");
 const getAdminToken = require("../../utils/getAdminToken");
 const mongoose = require("mongoose");
-const { createNewGame } = require("./utils/createNewGame");
+const { createNewGame, getNewGameObject } = require("./utils/createNewGame");
 const decodeToken = require("../../../utils/decodeToken");
 const dbDisconnection = require("../../../setup/dbDisconnection");
 const getHexedObjectId = require("../../../utils/getHexedObjectId");
@@ -67,6 +67,231 @@ describe(route, () => {
         const res = await exec(gameId);
 
         expect(res.body._id).toEqual(gameId);
+      });
+    });
+  });
+
+  describe("PUT", () => {
+    const { token } = getAdminToken();
+
+    describe("/:gameId", () => {
+      const exec = (gameId, changedGame) =>
+        request
+          .put(route + gameId)
+          .set("x-auth-token", token)
+          .send(changedGame);
+
+      test("if valid request, it will return 200", async () => {
+        const { gameId } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "b".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        changedGame = {
+          ...changedGame,
+          genreId: changedGame.genre._id,
+          developerId: changedGame.developer._id,
+        };
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        const res = await exec(gameId, changedGame);
+        expect(res.status).toBe(200);
+      });
+
+      test("if document was updated", async () => {
+        const { gameId } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "a".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        changedGame = {
+          ...changedGame,
+          genreId: changedGame.genre._id,
+          developerId: changedGame.developer._id,
+        };
+
+        const developer = changedGame.developer;
+        const genre = changedGame.genre;
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        await exec(gameId, changedGame);
+
+        changedGame.genre = genre;
+        changedGame.developer = developer;
+
+        delete changedGame.genreId;
+        delete changedGame.developerId;
+
+        const updatedGameInDB = await Game.findById(gameId);
+
+        expect({
+          price: updatedGameInDB.price,
+          title: updatedGameInDB.title,
+          description: updatedGameInDB.description,
+          genre: updatedGameInDB.genre.name,
+          developer: updatedGameInDB.developer.name,
+        }).toMatchObject({
+          price: changedGame.price,
+          title: changedGame.title,
+          description: changedGame.description,
+          genre: changedGame.genre.name,
+          developer: changedGame.developer.name,
+        });
+      });
+
+      test("if developer for provided developerId doesnt exists", async () => {
+        const { gameId } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "a".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        const randomObjectId = getHexedObjectId();
+
+        changedGame = {
+          ...changedGame,
+          genreId: changedGame.genre._id,
+          developerId: randomObjectId,
+        };
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        const res = await exec(gameId, changedGame);
+
+        expect(res.status).toEqual(404);
+      });
+      test("if genre for provided genreId doesnt exists", async () => {
+        const { gameId } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "a".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        const randomObjectId = getHexedObjectId();
+
+        changedGame = {
+          ...changedGame,
+          genreId: randomObjectId,
+          developerId: changedGame.developer._id,
+        };
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        const res = await exec(gameId, changedGame);
+
+        expect(res.status).toEqual(404);
+      });
+
+      test("if valid request, it will return updated document", async () => {
+        const { gameId, newGame } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "b".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        changedGame = {
+          ...changedGame,
+          genreId: changedGame.genre._id,
+          developerId: changedGame.developer._id,
+        };
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        const res = await exec(gameId, changedGame);
+
+        expect(res.body).not.toEqual(newGame);
+      });
+
+      test("if we pass empty request body, it wont change document", async () => {
+        const { gameId, newGame } = await createNewGame();
+
+        let changedGame = {};
+
+        await exec(gameId, changedGame);
+
+        const updatedGameInDB = await Game.findById(gameId);
+
+        expect({
+          price: updatedGameInDB.price,
+          title: updatedGameInDB.title,
+          description: updatedGameInDB.description,
+          genre: updatedGameInDB.genre.name,
+          developer: updatedGameInDB.developer.name,
+        }).toMatchObject({
+          price: newGame.price,
+          title: newGame.title,
+          description: newGame.description,
+          genre: newGame.genre.name,
+          developer: newGame.developer.name,
+        });
+      });
+
+      test("if valid request, it will set updatedDate", async () => {
+        const { gameId } = await createNewGame();
+
+        let changedGame = await getNewGameObject(
+          {
+            title: "New game title",
+            price: 28,
+            description: "b".repeat(26),
+          },
+          "New developer",
+          "New genre"
+        );
+
+        changedGame = {
+          ...changedGame,
+          genreId: changedGame.genre._id,
+          developerId: changedGame.developer._id,
+        };
+
+        delete changedGame.developer;
+        delete changedGame.genre;
+
+        await exec(gameId, changedGame);
+
+        const updatedGameInDB = await Game.findById(gameId);
+
+        expect(updatedGameInDB.updateDate).toBeDefined();
       });
     });
   });
