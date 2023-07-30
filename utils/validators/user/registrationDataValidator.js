3fb3f9e22097264cd97f39e532ceefc3da815797
babub22 <vladlesovoy2@gmail.com>
2023-07-30 10:41:26 +0200

    utils: Add validator for registration data

---
+++ utils/validators/user/registrationDataValidator.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+
+function registrationDataValidator(data) {
+  const schema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    username: Joi.string().required().min(3),
+  });
+
+  return schema.validate(data);
+}
+
+module.exports = registrationDataValidator;
