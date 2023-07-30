835390008341570df6d5e477d1dcbac3fbe60028
babub22 <vladlesovoy2@gmail.com>
2023-07-30 20:05:28 +0200

    models: Refactor `updateAverageScore` method

--- models/game/statics/updateAverageScore.js
+++ models/game/statics/updateAverageScore.js
@@ -1,10 +1,10 @@
 const getResultLikeResponseObject = require("../../../utils/getResultLikeResponseObject");
 const THIS_GAME_DOES_NOT_EXISTS = require("../../../utils/responseObjects/games/THIS_GAME_DOES_NOT_EXISTS");
 
-module.exports = async function ({ gameId, averageScore = 0 }) {
+module.exports = async function ({ gameId, averageScore}) {
   const updatedGame = await this.findByIdAndUpdate(
     gameId,
-    { averageScore: averageScore },
+    { averageScore },
     { new: true }
   );
 
