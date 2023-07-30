843771aa208f802e3525806f2fa649d35dee1c64
babub22 <vladlesovoy2@gmail.com>
2023-07-30 20:13:58 +0200

    developers: Add new route for `PUT/:developerId`

--- routes/developers.js
+++ routes/developers.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 const admin = require("../middleware/admin");
 const auth = require("../middleware/auth");
 const validateRequestBody = require("../middleware/validateRequestBody");
+const validateRequestParams = require("../middleware/validateRequestParams");
 const { Developer } = require("../models/developer");
+const THIS_DEVELOPER_ALREADY_EXISTS = require("../utils/responseObjects/developer/THIS_DEVELOPER_ALREADY_EXISTS");
+const THIS_DEVELOPER_DOES_NOT_EXISTS = require("../utils/responseObjects/developer/THIS_DEVELOPER_DOES_NOT_EXISTS");
 const developerValidator = require("../utils/validators/developer/developerValidator");
+const objectIdValidator = require("../utils/validators/objectIdValidator");
 
-router.get("/", async (req, res) => {
+router.get("/", async (_req, res) => {
   const developers = await Developer.find();
 
   res.send(developers);
@@ -17,9 +21,42 @@ router.post(
   async (req, res) => {
     const newDeveloper = new Developer(req.body);
 
+    const isDeveloperAlreadyExist = await Developer.exists(req.body);
+
+    if (isDeveloperAlreadyExist) {
+      const { status, message } = THIS_DEVELOPER_ALREADY_EXISTS;
+      return res.status(status).send(message);
+    }
+
     await newDeveloper.save();
 
-    res.send(newDeveloper);
+    res.status(201).send(newDeveloper);
+  }
+);
+
+router.put(
+  "/:objectId",
+  [
+    auth,
+    admin,
+    validateRequestParams(objectIdValidator),
+    validateRequestBody(developerValidator),
+  ],
+  async (req, res) => {
+    const { objectId: developerId } = req.params;
+
+    const updatedDeveloper = await Developer.findByIdAndUpdate(
+      developerId,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedDeveloper) {
+      const { status, message } = THIS_DEVELOPER_DOES_NOT_EXISTS;
+      return res.status(status).send(message);
+    }
+
+    res.send(updatedDeveloper);
   }
 );
 
