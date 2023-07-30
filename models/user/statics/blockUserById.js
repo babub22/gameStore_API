174b895d260d5e716be5f0b478e7ec51822dadbb
babub22 <vladlesovoy2@gmail.com>
2023-07-30 20:11:21 +0200

    models: Change behaviour of `blockUserById` method

--- models/user/statics/blockUserById.js
+++ models/user/statics/blockUserById.js
@@ -1,7 +1,17 @@
+const { isEqual } = require("lodash");
 const getResultLikeResponseObject = require("../../../utils/getResultLikeResponseObject");
 const USER_DOES_NOT_EXISTS = require("../../../utils/responseObjects/users/USER_DOES_NOT_EXISTS");
+const YOU_CANT_BLOCK_YOURSELF = require("../../../utils/responseObjects/users/YOU_CANT_BLOCK_YOURSELF");
 
 module.exports = async function ({ userId, currentUser, reason }) {
+  const isAdminTryingToBlockHimself = isEqual(currentUser._id, userId);
+
+  if (isAdminTryingToBlockHimself) {
+    return getResultLikeResponseObject({
+      errorObject: YOU_CANT_BLOCK_YOURSELF,
+    });
+  }
+
   const blockedUser = await this.findByIdAndUpdate(
     userId,
     {
