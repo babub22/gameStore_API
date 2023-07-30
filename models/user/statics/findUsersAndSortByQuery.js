28120c1569b59c02a293f556e45aa28f0b86a196
babub22 <vladlesovoy2@gmail.com>
2023-07-30 20:11:44 +0200

    models: Change behaviour of `findUsersAndSortByQuery` method

--- models/user/statics/findUsersAndSortByQuery.js
+++ models/user/statics/findUsersAndSortByQuery.js
@@ -1,3 +1,3 @@
 module.exports = function ({ query: { find, limit, sortBy } }) {
-  return this.find(find).limit(limit).sort(sortBy);
+  return this.find(find).limit(limit).sort(sortBy).select("-password");
 };
