2022196bc3d333f3d31712fdb64e002e99b45fa4
babub22 <vladlesovoy2@gmail.com>
2023-07-30 20:02:53 +0200

    users: Change behaviour of `getUser` handler

--- routes/users/handlers/getUser.js
+++ routes/users/handlers/getUser.js
@@ -1,17 +1,16 @@
-const { omit } = require("lodash");
 const { User } = require("../../../models/user/user");
 const USER_DOES_NOT_EXISTS = require("../../../utils/responseObjects/users/USER_DOES_NOT_EXISTS");
 
 async function getUser(req, res) {
   const { objectId: userId } = req.params;
 
-  const user = await User.findById(userId, { lean: true });
+  const user = await User.findById(userId).select("-password");
 
   if (!user) {
     return res.status(404).send(USER_DOES_NOT_EXISTS);
   }
 
-  res.send(omit(user, ["password"]));
+  res.send(user);
 }
 
 module.exports = getUser;
